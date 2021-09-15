ddbbbbf7501c2d42b73770839439a032ee5542c3
KitsuneMiko <serenajsaur@outlook.com>
2021-09-14 23:01:39 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -51,8 +51,8 @@ class CommentForm extends React.Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -116,7 +116,7 @@ function RenderCampsite({campsite}) { //Returns cards with the campsite image, t
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) { //If truthy, render comments with date/time, author and text.
         return(
             <div className="col-md-5 m-1">
@@ -126,7 +126,7 @@ function RenderComments({comments}) {
                         <div>{comment.text}</div>
                         <div>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} <br /><br /></div>
                     </div>))}
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>)
         }
         return <div />
@@ -149,7 +149,11 @@ function CampsiteInfo(props) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite ={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
                     </div>
                 </div>
             </div>
